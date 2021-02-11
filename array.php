a08eb805f41812831d01e6d5bc31658e64c033fa
KeiyaMorii <kei8.m.0123@gmail.com>
2021-02-11 15:58:12 +0900

    関数

--- array.php
+++ array.php
@@ -1,5 +1,14 @@
 <?php
-$dic = ["x"=>10, "y"=>20];
-$dic["y"] = 40;
-var_dump($dic);
+$file_list = [];
+function add_list($name){
+    global $file_list;
+    $file_name = $name.".php";
+    array_push($file_list, $file_name);
+}
+
+add_list("function");
+var_dump($file_list);
+
+add_list("hello");
+var_dump($file_list);
 ?>
